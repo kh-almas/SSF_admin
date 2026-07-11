90863541b91c501343b15ae6db044d6b4735d365
MD Almas Hossen <almaskh38@gmail.com>
2026-07-11 01:44:00 -0400

    change for live

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -92,12 +92,12 @@ function signupOrLogin(data) {
                 return;
             }
 
-            document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax`;
-            document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax`;
+            // document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax`;
+            // document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax`;
 
 //             for live
-//             document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
-//             document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
+            document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
+            document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
 
             window.sessionStorage.userId = res._id;
             window.sessionStorage.userToken = res.token;
