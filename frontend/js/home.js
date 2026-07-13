afbfc4d8359bd1a2125a0a2df10f36d199d2c55e
MD Almas Hossen <almaskh38@gmail.com>
2026-07-13 04:40:15 -0400

    fix bug

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -96,9 +96,8 @@ function signupOrLogin(data) {
             // document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax`;
 
 //             for live
-            document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
-            document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax; domain=.ssf.gov.bd`;
-
+            document.cookie = `userId=${encodeURIComponent(res._id)}; path=/; max-age=604800; SameSite=Lax domain=.ssf.gov.bd`;
+            document.cookie = `userToken=${encodeURIComponent(res.token)}; path=/; max-age=604800; SameSite=Lax domain=.ssf.gov.bd`;
             window.sessionStorage.userId = res._id;
             window.sessionStorage.userToken = res.token;
 
